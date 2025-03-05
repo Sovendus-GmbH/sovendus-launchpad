bd00f66d1277f13f3adb23edad0f19ffbcaebdbd
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-03-05 18:27:06 +0100

    Update release version to 1.0.1 in configuration file

--- sov_release.config.ts
+++ sov_release.config.ts
@@ -4,7 +4,7 @@ const releaseConfig: ReleaseConfig = {
   packages: [
     {
       directory: "./",
-      version: "0.0.1",
+      version: "1.0.1",
       release: true,
       lintAndBuild: true,
       test: false,
